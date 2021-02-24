a63c1be2151ece83b1e1d662007c5c63335a6e11
kakdidi <kakdidi@gmail.com>
2021-02-24 01:12:15 +0000

    tambah baris

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-class Hero: #template
-  def __init__(self, inputName, inputHealth, inputPower, inputArmor):
-    
+class Hero:  #template
+	def __init__(self, inputName, inputHealth, inputPower, inputArmor):
+		self.name = inputName
+		self.health = inputHealth
+		self.power = inputPower
+		self.armor = inputArmor
+
+
+hero1 = Hero("sniper", 100, 10, 4)
+print(hero1.__dict__)
