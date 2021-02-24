33168dc0337593758b5aed14039b514b9d3a7f38
kakdidi <kakdidi@gmail.com>
2021-02-24 05:04:17 +0000

    latihan method

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 class Hero:  #template
-	def __init__(self, inputName, inputHealth, inputPower, inputArmor):
-		self.name = inputName
-		self.health = inputHealth
-		self.power = inputPower
-		self.armor = inputArmor
+  #class variabel
+  jumlah = 0
+  def __init__(self, inputName, inputHealth, inputPower, inputArmor):
+	    #instance variabel
+		  self.name = inputName
+		  self.health = inputHealth
+		  self.power = inputPower
+		  self.armor = inputArmor
+		  Hero.jumlah += 1
+		  print("membuat Hero dengan nama " + inputName)
 
 
 hero1 = Hero("sniper", 100, 10, 4)
+print(Hero.jumlah)
 hero2 = Hero("mirana", 100, 15, 1)
+print(Hero.jumlah)
 hero3 = Hero("ucup", 1000, 100, 0)
+print(Hero.jumlah)
 print(hero1.__dict__)
 print(hero2.__dict__)
 print(hero3.__dict__)
